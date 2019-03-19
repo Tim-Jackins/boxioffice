ce487005d7a3504a4572c1d38f242a6ac6626b56
Tim-Jackins <jack@jacktimmins.com>
2019-03-19 16:54:01 -0400

    Removed whitenoise file caching

--- box_office/settings.py
+++ box_office/settings.py
@@ -169,7 +169,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static-only'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #For offline testing we used static folder outside the BASE_DIR
 '''
