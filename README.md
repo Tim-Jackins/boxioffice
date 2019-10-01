1c9142ed5385672d5228e31d5b585200e5fd6f53
Tim-Jackins <jack@jacktimmins.com>
2019-10-01 13:10:04 -0400

    Fixed automigrate function

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 
 > A combination online box office and actor database django webapp
 
+## Setup
+
+Create a .env file:
+
+```bash
+SECRET_KEY=
+ALLOWED_HOSTS=
+DEBUG=True
+ISLOCAL=True
+SOCIAL_AUTH_GITHUB_KEY=
+SOCIAL_AUTH_GITHUB_SECRET=
+AWS_STORAGE_BUCKET_NAME=
+AWS_S3_REGION_NAME=
+AWS_S3_ENDPOINT_URL=
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+MAILGUN_API_KEY=
+MAILGUN_SMTP_SERVER=
+MAILGUN_SMTP_LOGIN=
+MAILGUN_SMTP_PASSWORD=
+MAILGUN_BASE_URL=
+STRIPE_SECRET_KEY=
+STRIPE_PUBLISHABLE_KEY=
+```
+
+Setup the db:
+
+You should be able to simply run the `automigrate.py` program.
+
 ## Handy notes
 
 [Cookbook](https://github.com/nigma/heroku-django-cookbook/blob/master/README.md)
--- booking/urls.py
+++ booking/urls.py
@@ -1,3 +1,27 @@
 
-from django.conf.urls import url
+from django.urls import path
 from . import views
+'''
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('admintools/', views.AdminTools, name='adminTools'),
+    path('tickets/', views.listTickets, name='ticket_list'),
+]
+
+urlpatterns = [
+    
+]
+
+url(r'^booking/$', booking_views.show_index,name='booking'),
+url(r'^booking/(?P<pk>\d+)/$', booking_views.ShowDetails.as_view(), name='show_details'),
+
+url(r'^booking/seatchoice/(?P<pk>\d+)/$', booking_views.reserve_seat, name='reserve_seat'),
+url(r'^booking/payment/$', booking_views.payment_gateway, name='payment_gateway'),
+
+url(r'^booking/confirmation/$', booking_views.payment_confirmation, name='payment_confirmation'),
+url(r'^booking/theater/seats/(?P<pk>\d+)/$', booking_views.TheaterSeatsJSON, name='theater_seats'),
+#Hard Coded templates i.e. without views.
+url(r'^booking/payment/booking/seatnotfound.html$', TemplateView.as_view(template_name="booking/seatnotfound.html"), name='seatnotfound'),
+url(r'^booking/payment_confirmation/booking/seatconflict.html$', TemplateView.as_view(template_name="booking/seatconflict.html"), name='seatconflict'),
+url(r'^booking/payment_confirmation/booking/payment_cancelled.html$', TemplateView.as_view(template_name="booking/payment_cancelled.html"), name='payment_cancelled'),
+'''
