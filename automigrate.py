dd8c79c9da86856e07db106bb6a5208ec2553e90
Jack <jack@jacktimmins.com>
2019-10-02 10:42:18 -0400

    Removed paypal

--- automigrate.py
+++ automigrate.py
@@ -1,8 +1,9 @@
 from glob import glob
 from os import system
 import platform
+import sys
 
 if platform.system() == 'Windows':
     for i in glob('*/models.py'):
-        system(f'py manage.py makemigrations {i[:-10]}')
-    system('py manage.py migrate')
+        system(f'{sys.executable} manage.py makemigrations {i[:-10]}')
+    system(f'{sys.executable} manage.py migrate')
