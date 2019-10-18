823e20e586d8948a538874994d40252c81e736f2
Jack <jack@jacktimmins.com>
2019-10-17 23:46:03 -0400

    moved dotenv import

--- boxioffice/settings.py
+++ boxioffice/settings.py
@@ -5,11 +5,6 @@ from decouple import config
 import dj_database_url
 
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-
 # Misc
 
 FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
@@ -18,6 +13,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
